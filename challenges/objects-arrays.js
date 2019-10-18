8ea4d9c0fc7f43f62eb50e81850bd2610072ff7f
Your Name <you@example.com>
2019-10-18 11:46:14 -0400

    log results for contact first name and email

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -13,8 +13,11 @@ const dino1 ={
   diet: `carnivorous`,
   weight: `7000kg`,
   length: `12m`,
-  period: `Late Cretaceous`
-}
+  period: `Late Cretaceous`,
+  roar: function(){
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+};
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
@@ -39,20 +42,20 @@ const dino3 ={
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dino1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dino3.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dino2.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dino1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dino1.roar());
 
 
 // ==== Arrays ====
@@ -76,7 +79,8 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities);
+  graduates.forEach(school => universities.push(school.university)),
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -85,6 +89,7 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+  graduates.forEach (contact => contactInfo.push(`${contact.first_name} ${contact.email}`));
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
