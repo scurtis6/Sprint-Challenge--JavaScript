4fa541f2c8f1213dfb5befb7fdb1b2d79b1946c2
Sierra Curtis <sierracurtis91@gmail.com>
2019-11-15 11:28:19 -0500

    finished problems for objects

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,10 +1,5 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-class CuboidMakerClass {
-    constructor(attributes){
-        this.length = attributes.length,
-        this.width = attributes.width,
-        this.height = attributes.height
-  }
+
 
   /* == Step 2: Volume Method ==
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
@@ -12,9 +7,7 @@ class CuboidMakerClass {
   Formula for cuboid volume: length * width * height
 */
 
-  volume() { 
-    return this.length * this.width * this.height;
-  }
+ 
 
   /* == Step 3: Surface Area Method ==
   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
@@ -22,44 +15,17 @@ class CuboidMakerClass {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
-  surfaceArea() {
-    return (2 * (this.length * this.width + this.length * this.height + this.width * this.height));
-  } 
-};
+ 
 
   /* == Step 4: Create a new object that uses CuboidMaker ==
     Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
     Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
   */
   
-  const cuboid2 = new CuboidMaker({
-    length: 4,
-    width: 5,
-    height: 5
-  });
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
-class CubeMaker extends CuboidMakerClass {
-    constructor(attributes){
-        super(attributes);
-  }
-  volume() { 
-    return this.length * this.width * this.height;
-  }
-  surfaceArea() {
-    return (6 * (this.length * this.length));
-  } 
-};
-
-const cube = new CubeMaker({
-    length: 5,
-    width: 5,
-    height: 5
-  });
-
-  console.log(cube.volume());
-  console.log(cube.surfaceArea());
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,31 +8,31 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
-const dino1 ={
-  name: `Tyrannosaurus`,
+const dino1 = {
+  name: `tyrannosaurus`,
   diet: `carnivorous`,
   weight: `7000kg`,
   length: `12m`,
-  period: `Late Cretaceous`,
-  roar: function(){
-    return "RAWERSRARARWERSARARARRRR!";
-  }
-};
+  period: `Late Cretaceous`
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
-const dino2 ={
-  name: `Stegosaurus`,
+const dino2 = {
+  name: `stegosaurus`,
   diet: `herbivorous`,
   weight: `2000kg`,
   length: `9m`,
-  period: `Late Jurassic`
+  period: `Late Jurassic`,
+  roar: function(){
+    return `RAWERSRARARWERSARARARRRR!`
+  }
 }
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
-const dino3 ={
-  name: `Velociraptor`,
+const dino3 = {
+  name: `velociraptor`,
   diet: `carnivorous`,
   weight: `15kg`,
   length: `1.8m`,
@@ -42,21 +42,20 @@ const dino3 ={
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log(dino1.weight);
+console.log(dino1.weight());
 
 // What was the diet of a velociraptor?
-console.log(dino3.diet);
+console.log(dino3.diet());
 
 // How long was a stegosaurus?
-console.log(dino2.length);
+console.log(dino2.length());
 
 // What time period did tyrannosaurus live in?
-console.log(dino1.period);
+console.log(dino1.period());
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log(dino1.roar());
-
+console.log(dino1.roar())
 
 // ==== Arrays ====
 
@@ -78,9 +77,7 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-  graduates.forEach(school => universities.push(school.university)),
-console.log(universities.sort());
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -88,13 +85,10 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
-  graduates.forEach (contact => contactInfo.push(`${contact.first_name} ${contact.email}`));
-console.log(contactInfo);
+
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = graduates.filter ((three) => three.university.includes ('Uni'))
-console.log(unisWithUni);
+
 
 
 // ==== ADVANCED Array Methods ====
@@ -119,9 +113,7 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
-  zooAnimals.forEach (names => displayNames.push(`${names.animal_name} ${names.scientific_name}`))
-console.log(displayNames);
+
 
 /* Request 2: .map()
 
@@ -129,27 +121,21 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-let lowCaseAnimalNames = [];
-lowCaseAnimalNames = zooAnimals.map(animal => animal.animal_name.toLowerCase());
-console.log(lowCaseAnimalNames);
+
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-let lowPopulationAnimals = [];
-lowPopulationAnimals = zooAnimals.filter(pop => pop.population < 5);
-console.log(lowPopulationAnimals);
+
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
-  populationTotal = zooAnimals.reduce((accumulator, currentValue) => accumulator + (currentValue.population),0);
-console.log(populationTotal);
+
 
 
 /*
